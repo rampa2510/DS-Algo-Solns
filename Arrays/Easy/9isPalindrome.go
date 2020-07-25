1a7f0c9510570554e05d30166ea8e4818c50c118
rampa2510 <iamram2510@gmail.com>
2020-07-25 19:25:20 +0530

    Added solution for palindrome problem

---
+++ Arrays/Easy/9isPalindrome.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func isPalindrome(k int) bool {
+
+	var rev int = 0
+	var r int = 0
+	if k < 0 {
+		return false
+	}
+	num := k
+	for num > 0 {
+		r = num % 10
+		rev = rev*10 + r
+		num = num / 10
+	}
+	return rev == k
+}
+
+func main() {
+	a := isPalindrome(10)
+	fmt.Println(a)
+}
