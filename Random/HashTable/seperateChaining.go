c2a1b5b5c60ead85340110b529ea78ea51d2e6dd
rampa2510 <iamram2510@gmail.com>
2020-07-28 16:08:50 +0530

    Completed seperate chaining for static size

--- Random/HashTable/seperateChaining.go
+++ Random/HashTable/seperateChaining.go
@@ -32,7 +32,7 @@ func (ht *hashTable) searching(key int) int {
 	elementsAtIdx = &ht.values[key%7]
 	if elementsAtIdx.taken != true {
 		fmt.Printf("No element with %v as a key exists", key)
-		return 0
+		return -1
 	}
 	// for elementAtIdx.n
 	for elementsAtIdx != nil {
@@ -42,7 +42,49 @@ func (ht *hashTable) searching(key int) int {
 		elementsAtIdx = elementsAtIdx.next
 	}
 	fmt.Printf("No element with %v as a key exists", key)
-	return 0
+	return -1
+}
+
+func (ht *hashTable) deletion(key int) int {
+	idx := key % 7
+	elementAtIdx := &ht.values[idx]
+	if elementAtIdx.taken != true {
+		fmt.Printf("No element with %v as a key exists", key)
+		return -1
+	}
+	var prevElement *keyValue
+	prevElement = &keyValue{key: -1}
+	for elementAtIdx != nil {
+		if elementAtIdx.key == key {
+			break
+		}
+		prevElement = elementAtIdx
+		elementAtIdx = elementAtIdx.next
+	}
+	// fmt.Println(elementAtIdx, prevElement)
+	if elementAtIdx == nil {
+		fmt.Printf("No element with %v as a key exists", key)
+		return -1
+	}
+	if prevElement.key == -1 && elementAtIdx.next != nil {
+		ht.values[idx] = *elementAtIdx.next
+		return elementAtIdx.value
+	}
+	if prevElement.key != -1 {
+		prevElement.next = elementAtIdx.next
+		return elementAtIdx.value
+	}
+	ht.values[idx] = keyValue{}
+	// fmt.Println(prevElement.next, elementAtIdx.next)
+	return elementAtIdx.value
+}
+
+func (ht *hashTable) display(key int) {
+	elementAtIdx := &ht.values[key%7]
+	for elementAtIdx != nil {
+		fmt.Println(elementAtIdx.value)
+		elementAtIdx = elementAtIdx.next
+	}
 }
 
 func main() {
@@ -66,6 +108,19 @@ func main() {
 	n = ht1.searching(13)
 	fmt.Println(n)
 	n = ht1.searching(20)
-	fmt.Println(n)
-
+	// fmt.Println(n, ht1.values)
+	fmt.Println("***************Values before deletion of 1****************")
+	fmt.Println(ht1.values)
+	fmt.Println("************************************")
+	fmt.Println("***************Values after deletion of 1****************")
+	n = ht1.deletion(1)
+	fmt.Println(ht1.values)
+	fmt.Println("************************************")
+	fmt.Println("***************Value before deletion of tree****************")
+	ht1.display(6)
+	fmt.Println("************************************")
+	n = ht1.deletion(20)
+	fmt.Println("***************Value after deletion of tree****************")
+	ht1.display(6)
+	fmt.Println("************************************")
 }
